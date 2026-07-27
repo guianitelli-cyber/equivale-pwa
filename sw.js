ea56de24e1e59a98ec0ca9540a60f461bc692411
Guilherme Anitelli <guianitelli@gmail.com>
2026-07-26 22:30:25 -0300

    troca estrategia de cache para sempre buscar versao mais nova

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'equivale-v1';
+const CACHE_NAME = 'equivale-v3';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -24,12 +24,12 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
-      }).catch(() => cached);
-    })
+      })
+      .catch(() => caches.match(event.request))
   );
 });
